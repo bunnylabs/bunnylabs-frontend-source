a92d48e14ce934a0647641881b51f7ba1ae7f749
David Siaw <davidsiaw@gmail.com>
2015-05-15 12:14:17 +0900

    add resources, include only directories in Applications folder

--- config.rb
+++ config.rb
@@ -45,7 +45,7 @@ helpers do
   
   # Returns all applications' names
   def application_list
-    Dir['source/localizable/Frameworks/Applications/*'].map { |a| File.basename(a) }
+    Dir['source/localizable/Applications/*'].select { |a| File.directory? a }.map { |a| File.basename(a) }
   end
 
 end
